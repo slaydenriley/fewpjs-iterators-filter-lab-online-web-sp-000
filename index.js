468a759c2b448c849b540814447aa40eaa1cc31e
Riley Slayden <slaydenriley@gmail.com>
2020-08-16 15:02:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function findMatching (list, name) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 };
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
+}
